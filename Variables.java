1402e93c555d468bb2e9d9be37ef9deb1de27053
dhanush4300 <dhanush4300@gmail.com>
2025-07-27 19:49:14 +0530

    first commit

---
+++ Variables.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Variables {
+    public static void main(String[] args) {
+        String Name = "dhanush";
+        int Age = 18;
+        char Gender = 'M';
+        System.out.println("Name:"+Name+" Age"+ Age+" Gender "+ Gender);
+        
+    }
+}
